77c7facdd9542d255f19f08469c21e71c5268a89
Abdullah_J <abdullahj1899@gmail.com>
2023-08-01 20:33:32 -0400

    Added fetching public repo list

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 //User overview div
 const overview = document.querySelector(".overview");
+//Repository list ul
+const repoList = document.querySelector(".repo-list");
 const username = "AbdullahJ99";
+
+
 const profile = async function(){
     const res = await fetch(`https://api.github.com/users/${username}`);
     const profileDetails = await res.json();
     displayUser(profileDetails);
 }
+
+profile();
+
 const displayUser = function(profile){
     const userInfo = document.createElement("div");
     userInfo.classList.add("user-info");
@@ -20,6 +27,20 @@ const displayUser = function(profile){
         <p><strong>Number of public repos:</strong> ${profile.public_repos}</p>
     </div>`;
     overview.append(userInfo);
+    repos();
 }
 
-profile();
+const repos = async function(){
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repo = await res.json();
+    displayRepos(repo);
+}
+
+const displayRepos = function(repos){
+    for(const repo of repos){
+        let li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    }
+}
